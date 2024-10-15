ce3241458ab78a795cba98668c60c35741aad3c8
Beliakov Vladimir <ermegilius@gmail.com>
2024-10-15 18:26:20 +0300

    end of the day 15.10.2024

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 
 document.getElementById("year").innerHTML = new Date().getFullYear();
 
-// window.on("scroll", function () {
-//     if (window.scrollTop() > 50) {
-//         document.querySelector("header").addClass("colored");
-//     } else {
-//         document.querySelector("header").removeClass("colored");
-//     }
-// });
-
 //back to top function
 function back() {
     window.scrollTo({
