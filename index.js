916a26b5faff2ca32c08f5f2cd39cf113dbcec5f
Beliakov Vladimir <ermegilius@gmail.com>
2024-10-15 18:23:01 +0300

    add: back to top button

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+// window.on("scroll", function () {
+//     if (window.scrollTop() > 50) {
+//         document.querySelector("header").addClass("colored");
+//     } else {
+//         document.querySelector("header").removeClass("colored");
+//     }
+// });
+
+//back to top function
+function back() {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+}
+
+document.querySelector("#backToTop").addEventListener('click', back);
