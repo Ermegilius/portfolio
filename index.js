71571eca31f0452edde6020d78c07b67690e20a7
Beliakov Vladimir <ermegilius@gmail.com>
2024-10-16 15:54:07 +0300

    fix: nav buttons scroll. Now it shifts with the header heigth. Add: monochrom filter to the header on scroll

--- index.js
+++ index.js
@@ -10,4 +10,46 @@ function back() {
     });
 }
 
-document.querySelector("#backToTop").addEventListener('click', back);
+//header background color on scroll function
+function scrolled() {
+    const header = document.querySelector("header");
+    if (window.scrollY > 50) {
+        header.style.filter = 'grayscale(1)';
+        console.log('scrolled');
+    } else {
+        header.style.filter = 'grayscale(0)';
+        console.log('unscrolled');
+    }
+}
+
+function debounce(func, delay) {
+    let inDebounce;
+    return function () {
+        const context = this;
+        const args = arguments;
+        clearTimeout(inDebounce);
+        inDebounce = setTimeout(() => func.apply(context, args), delay);
+    }
+}
+
+
+//shifts anchor positions in the document with current heigth of the header (used chatGPT to write this function)
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        let headerHeigth = document.querySelector('header').offsetHeight;
+        const offset = headerHeigth;
+        const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY - offset;//(position of the desired element relative to the viewport) + (current viewport shift along Y axis) - (current heigth of the header)
+
+        window.scrollTo({
+            top: targetPosition,
+            behavior: 'smooth',
+        });
+    });
+});
+
+document.querySelector('#backToTop').addEventListener('click', back);
+window.addEventListener('scroll', debounce(scrolled, 500));
